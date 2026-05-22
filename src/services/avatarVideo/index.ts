f5753ceba7ed86ba9d0304579cfae8171c15007f
baofeng <baofeng@baofengdeMacBook-Pro.local>
2026-05-22 16:05:02 +0800

    chore: remove renderer-side avatar-video queue and Tencent provider

--- src/services/avatarVideo/index.ts
+++
@@ -1,5 +0,0 @@
-import { TencentAvatarVideoProvider } from './TencentAvatarVideoProvider'
-import type { AvatarVideoProvider } from './AvatarVideoProvider'
-
-export const avatarVideoProvider: AvatarVideoProvider = new TencentAvatarVideoProvider()
-export type { AvatarVideoProvider }
